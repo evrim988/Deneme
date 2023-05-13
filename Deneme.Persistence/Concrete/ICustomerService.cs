290da048978f374225234f98a48658210a458651
EtkinBilgi-desktop <user@DESKTOP-J6CI1S6>
2023-05-13 10:41:00 +0300

    Add project files.

---
+++ Deneme.Persistence/Concrete/ICustomerService.cs
@@ -0,0 +1,22 @@
+﻿using Deneme.Application.Abstractions;
+using Deneme.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme.Persistence.Concrete
+{
+    internal class ICustomerService : ICostumerServices
+    {
+        public List<Customer> GetCustomers()
+            => new() 
+            {
+                new() { id=1,Name="Evrim",Surname="Çalışkan",CostumerID=1 },
+                new() { id=2,Name="Fatma",Surname="Aslan",CostumerID=1 },
+                new() { id=3,Name="Ayşe",Surname="Çalışkan",CostumerID=1 },
+                new() { id=4,Name="Ali",Surname="Çalışkan",CostumerID=1 }
+            };
+    }
+}
